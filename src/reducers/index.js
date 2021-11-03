a9d436187725625b2cd261c7394ead80758487ab
Alexander Dashko <leksdashko@gmail.com>
2021-11-04 00:27:01 +0200

    task136 update item in the cart

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,9 +7,6 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-
-    console.log(action.type);
-
     switch(action.type){
 
         case 'FETCH_BOOKS_REQUEST':
@@ -35,7 +32,28 @@ const reducer = (state = initialState, action) => {
             }
         case 'BOOK_ADDED_TO_CART':
             const bookId = action.payload;
+            const cartBook = state.cartItems.find((book) => book.id === bookId);
             const book = state.books.find((book) => book.id === bookId);
+
+            if(cartBook){
+                const cartItems = [];
+                state.cartItems.map((item) => {
+                    if(item.id === bookId){
+                        item = {
+                            ...item,
+                            count: ++item.count,
+                            total: item.total + book.price
+                        };
+                    }
+                    cartItems.push(item);
+                });
+
+                return {
+                    ...state,
+                    cartItems: cartItems
+                }
+            }
+            
             const newItem = {
                 id: book.id,
                 title: book.title,
